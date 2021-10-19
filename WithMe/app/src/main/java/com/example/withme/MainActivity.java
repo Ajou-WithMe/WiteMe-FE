fa6ac92c13b53f01f26c9b99c50eb8a12e850ff2
yu04038 <yu04038@gmail.com>
2021-10-19 15:34:27 +0900

    네이버 지도 현재 위치 받아오기

--- WithMe/app/src/main/java/com/example/withme/MainActivity.java
+++ WithMe/app/src/main/java/com/example/withme/MainActivity.java
@@ -3,7 +3,9 @@ package com.example.withme;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -16,24 +18,37 @@ import com.example.withme.intro.DescriptionActivity;
 import com.example.withme.user.LoginActivity;
 import com.kakao.usermgmt.UserManagement;
 import com.kakao.usermgmt.callback.LogoutResponseCallback;
+import com.naver.maps.map.LocationTrackingMode;
 import com.naver.maps.map.MapView;
 import com.naver.maps.map.NaverMap;
 import com.naver.maps.map.OnMapReadyCallback;
 import com.naver.maps.map.UiSettings;
+import com.naver.maps.map.util.FusedLocationSource;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1000;
+
+    private static final String[] PERMISSION = {
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION
+    };
 
     private ConstraintLayout coachMark;
     private ImageButton makeGroup1, makeGroup2;
     private NaverMap naverMap;
     private MapView mapView;
     private Button logout;
+    private FusedLocationSource fusedLocationSource;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fusedLocationSource = new FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE);
 
         Intent intent = new Intent(this, GroupActivity1.class);
         Intent intent1 = new Intent(this, DescriptionActivity.class);
@@ -85,11 +100,29 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     }
 
     @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (fusedLocationSource.onRequestPermissionsResult(requestCode, permissions, grantResults)) {
+            if(!fusedLocationSource.isActivated()) { //권한 거부됨
+                naverMap.setLocationTrackingMode(LocationTrackingMode.None);
+            } else {
+                naverMap.setLocationTrackingMode(LocationTrackingMode.Follow);
+            }
+            return;
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @Override
     public void onMapReady(@NonNull NaverMap naverMap) {
         UiSettings uiSettings = naverMap.getUiSettings();
         uiSettings.setCompassEnabled(false);
         uiSettings.setScaleBarEnabled(false);
         uiSettings.setZoomControlEnabled(false);
         uiSettings.setRotateGesturesEnabled(false);
+
+        this.naverMap = naverMap;
+
+        naverMap.setLocationSource(fusedLocationSource); //현재 위치
+        ActivityCompat.requestPermissions(this, PERMISSION, LOCATION_PERMISSION_REQUEST_CODE);
     }
 }
